1c05ef8824646cb9a50fbc338b53c8f6f3424943
SyedNaveethS <syed@noukha.in>
2026-05-19 15:08:18 +0530

    feat: add authentication login page

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,5 +1,6 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { ROUTES } from './routes';
+import LoginPage from '../pages/auth/LoginPage';
 import NotFoundPage from '../pages/NotFoundPage';
 import UnauthorizedPage from '../pages/UnauthorizedPage';
 
@@ -12,7 +13,8 @@ const AppRouter = () => {
     <BrowserRouter>
       <Routes>
         {/* Public routes */}
-        {/* <Route path={ROUTES.LOGIN} element={<LoginPage />} /> */}
+        <Route path={ROUTES.LOGIN} element={<LoginPage />} />
+        <Route path={ROUTES.HOME} element={<Navigate to={ROUTES.LOGIN} replace />} />
 
         {/* Protected routes */}
         {/* <Route path={ROUTES.DASHBOARD} element={<PrivateRoute><DashboardPage /></PrivateRoute>} /> */}
